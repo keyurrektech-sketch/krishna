2adbcccf6c92f30c4dda7de4275ca4fc43cc85a4
keyur <keyur.rektech@gmail.com>
2025-09-20 14:22:40 +0530

    Court Case

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,12 +7,12 @@
 			<div class="container">
 				<div class="row row-cols-1 row-cols-lg-2 row-cols-xl-3">
 					<div class="col mx-auto">
+						<div class="mb-3 text-center">
+							<img src="{{ $settings && $settings->logo ? asset('uploads/'.$settings->logo) : asset('uploads/user.png')}}" width="60" alt="" style="width: 180px;">
+						</div>
 						<div class="card mb-0">
 							<div class="card-body">
 								<div class="p-4">
-									<div class="mb-3 text-center">
-										<img src="{{ $settings && $settings->logo ? asset('uploads/'.$settings->logo) : asset('uploads/user.png')}}" width="60" alt="" />
-									</div>
 									<div class="text-center mb-4">
 										<h5 class="">Krishna Minerals Admin</h5>
 										<p class="mb-0">Please log in to your account</p>
